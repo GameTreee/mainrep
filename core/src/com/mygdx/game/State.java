186d8f19b6f2be7db8c402ce9d727de3158da9fb
chester5009 <pauletto1234@gmail.com>
2016-02-04 03:16:24 +0300

    "change from ubuntu"

---
+++ core/src/com/mygdx/game/State.java
@@ -0,0 +1,19 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by chester on 04.02.16.
+ */
+public class State {
+    public State(){
+
+    }
+
+    protected void update(){
+
+    }
+    protected void draw(SpriteBatch spriteBatch){
+
+    }
+}
