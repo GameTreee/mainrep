c4afd21d85df0257fe2430ae19da30d17a279571
chester5009 <pauletto1234@gmail.com>
2016-02-06 04:26:27 +0300

    last

--- core/src/com/mygdx/game/MenuState.java
+++ core/src/com/mygdx/game/MenuState.java
@@ -3,15 +3,28 @@ package com.mygdx.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+import java.awt.Rectangle;
+import java.security.Key;
 
 /**
  * Created by CHESTER on 04.02.2016.
  */
 public class MenuState extends State {
+
+
+
     private Texture bg;
+    private Texture buttons;
+    private TextureRegion startBtn,exitBtn;
+    private Rectangle startRect,exitRect;
     public MenuState(StateManager sm){
         super(sm);
         bg=new Texture("bg.png");
+        buttons=new Texture("buttons.png");
+        startBtn=new TextureRegion(buttons,0,0,600,150);
+        exitBtn=new TextureRegion(buttons,0,150,600,150);
     }
     @Override
     protected void handleInput() {
@@ -27,7 +40,13 @@ public class MenuState extends State {
     public void render(SpriteBatch sb) {
         Gdx.gl.glClearColor(0,0,0,1);
         sb.begin();
-        sb.draw(bg,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+        sb.draw(bg, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        sb.draw(startBtn, MyGdxGame.WIDTH / 2 - startBtn.getRegionWidth() / 2 * 0.35f,
+                MyGdxGame.HEIGHT / 2 + 50, 0, 0, startBtn.getRegionWidth(), startBtn.getRegionHeight(), 0.35f, 0.65f, 0);
+        sb.draw(exitBtn,MyGdxGame.WIDTH/2-exitBtn.getRegionWidth()/2*0.35f,
+                MyGdxGame.HEIGHT/2-150,0,0,exitBtn.getRegionWidth(),exitBtn.getRegionHeight(),0.35f,0.65f,0);
+        /*sb.draw(exitBtn,MyGdxGame.WIDTH/2-150,
+                MyGdxGame.HEIGHT/2,300,100);*/
         sb.end();
     }
 
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -7,6 +7,10 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGdxGame extends ApplicationAdapter {
+
+	public static final int WIDTH=640;
+	public static final int HEIGHT=480;
+
 	private SpriteBatch batch;
 	private StateManager sm;
 	@Override
@@ -14,7 +18,7 @@ public class MyGdxGame extends ApplicationAdapter {
 		batch = new SpriteBatch();
 		sm=new StateManager();
 		Gdx.gl.glClearColor(0.35f, 0.33f, 0.87f, 1);
-		Gdx.graphics.setWindowedMode(640,480);
+		Gdx.graphics.setWindowedMode(WIDTH,HEIGHT);
 		sm.push(new MenuState(sm));
 	}
 
